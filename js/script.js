987297333770ab92dac2195a91ff133561493cb7
Alex Shi <alexshi@lx-user-machine.local>
2019-08-27 17:57:12 -0400

    content

---
+++ js/script.js
@@ -0,0 +1,184 @@
+// since this is a static site, keep this file as small as possible
+
+
+
+
+// FUNCTIONAL & NAVIGATIONAL
+
+
+
+
+function scrolldown(target, speed, offset_ratio) {
+  $('html, body').animate({
+    scrollTop: $(target).offset().top * offset_ratio
+  }, speed);
+}
+
+function scrollup(target, speed) {
+  $('html, body').animate({
+    scrollTop: $(target).offset().top
+  }, speed);
+}
+
+function resumePopup() {
+  //TODO
+}
+
+function resumeDownload() {
+  //TODO
+}
+
+
+
+
+// PROJECT SECTION SCROLL EFFECT
+
+
+
+
+window.addEventListener('scroll', function() {
+  var element = document.querySelector('.project-wrapper');
+  var position = element.getBoundingClientRect();
+
+  // checking whether fully visible
+  if(position.top >= 0 && position.bottom <= window.innerHeight) {
+  }
+
+  // checking for partial visibility
+  if(position.top < window.innerHeight && position.bottom >= 0) {
+    var mosaic = document.querySelector('#mosaic');
+    mosaic.className = mosaic.className.replace(
+      mosaic.className,
+      'bg-scroll'
+    );
+  }
+  else {
+    var mosaic = document.querySelector('#mosaic');
+    mosaic.className = mosaic.className.replace(
+      mosaic.className,
+      'bg-fixed'
+    );
+  }
+});
+
+
+
+
+// ANIMATION
+
+
+
+
+function load_animators() {
+  load_progress();
+  load_timeline();
+  load_projects();
+}
+
+function load_progress() {
+  var progressor = function() {
+    var elems;
+    var windowHeight;
+    function init() {
+      elems = document.querySelectorAll('.skill');
+      windowHeight = window.innerHeight;
+      addEventHandlers();
+      checkPosition();
+    }
+    function addEventHandlers() {
+      window.addEventListener('scroll', checkPosition);
+      window.addEventListener('resize', init);
+    }
+    function checkPosition() {
+      for (var i = 0; i < elems.length; i++) {
+        var positionFromTop = elems[i].getBoundingClientRect().top;
+        if (positionFromTop - windowHeight <= 0) {
+          elems[i].className = elems[i].className.replace(
+            elems[i].className.split(' ')[2],
+            elems[i].className.split(' ')[1] + '-after'
+          );
+        }
+      }
+    }
+    return {
+      init: init
+    };
+  };
+  progressor().init();
+}
+
+function load_timeline() {
+  var progressor = function() {
+    var elems;
+    var windowHeight;
+    function init() {
+      elems = document.querySelectorAll('.timeline-entry');
+      windowHeight = window.innerHeight;
+      addEventHandlers();
+      checkPosition();
+    }
+    function addEventHandlers() {
+      window.addEventListener('scroll', checkPosition);
+      window.addEventListener('resize', init);
+    }
+    function checkPosition() {
+      for (var i = 0; i < elems.length; i++) {
+        var positionFromTop = elems[i].getBoundingClientRect().top;
+        if (positionFromTop - windowHeight <= 0) {
+          elems[i].className = elems[i].className.replace(
+            elems[i].className.split(" ")[1],
+            elems[i].className.split(" ")[0] + "-after"
+          );
+        }
+      }
+    }
+    return {
+      init: init
+    };
+  };
+  progressor().init();
+}
+
+function load_projects() {
+  var progressor = function() {
+    var elems;
+    var windowHeight;
+    function init() {
+      elems = document.querySelectorAll('.project-card');
+      windowHeight = window.innerHeight;
+      addEventHandlers();
+      checkPosition();
+    }
+    function addEventHandlers() {
+      window.addEventListener('scroll', checkPosition);
+      window.addEventListener('resize', init);
+    }
+    function checkPosition() {
+      for (var i = 0; i < elems.length; i++) {
+        var positionFromTop = elems[i].getBoundingClientRect().top;
+        if (positionFromTop - windowHeight <= -windowHeight / 4 ) {
+          if (i % 2 === 0) {
+            elems[i].className = elems[i].className.replace(
+              elems[i].className.split(' ')[1],
+              'project-card-after-right'
+            );
+            elems[i].className = elems[i].className.replace(
+              elems[i].className.split(' ')[2],
+              'project-card-visible'
+            );
+          }
+          else {
+            elems[i].className = elems[i].className.replace(
+              elems[i].className.split(' ')[1],
+              'project-card-after-left'
+            );
+          }
+        }
+      }
+    }
+    return {
+      init: init
+    };
+  };
+  progressor().init();
+}
