3d3a95c1f7624f109eaa1f568efa10ca0f41b4a5
alexshi0000 <yuanshi948@gmail.com>
2020-01-02 18:34:15 -0500

    .

--- js/dir.js
+++ js/dir.js
@@ -1,11 +1,58 @@
-let height = window.innerHeight;
-let width = window.innerWidth;
+function preloader() {
+	if (document.images) {
+		var urls = [
+			"./images/ipad-main-noglasses.jpg",
+			"./images/main-bg-min.jpg",
+			"./images/cofplant.png",
+			"./images/floating-desk-min.jpg",
+			"./images/galaxy-optimized.gif",
+			"./images/dualmetro.jpg",
+			"./images/chat.png",
+			"./images/tetris.gif",
+			"./images/fractree.jpg",
+			"./images/stallman-min.png",
+			"./images/autodeck.jpg",
+			"./images/emotion.jpg",
+			"./images/redphone.jpeg"
+		]
+		var imgs = [];
+		let n = urls.length;
+		for (var i = 0; i < n; i++)
+			imgs.push(new Image());
 
-if (height <= width) { //simple redirection
-	console.log("redirected to desktop version");
-	window.location.replace("home.html");
+		for (var i = 0; i < n; i++)
+			imgs[i].src = urls[i];
+
+		console.log(imgs);
+	}
 }
-else {
-	console.log("redirected to mobile version");
-	window.location.replace("mobile.html");
+
+function addLoadEvent(func) {
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function') {
+		window.onload = func;
+	} else {
+		window.onload = function() {
+			if (oldonload) {
+				oldonload();
+			}
+			func();
+		}
+	}
 }
+
+function redir() {
+	let height = window.innerHeight;
+	let width = window.innerWidth;
+
+	if (height <= width) { //simple redirection
+		console.log("redirected to desktop version");
+		window.location.replace("home.html");
+	}
+	else {
+		console.log("redirected to mobile version");
+		window.location.replace("mobile.html");
+	}
+}
+
+addLoadEvent(preloader, redir());
