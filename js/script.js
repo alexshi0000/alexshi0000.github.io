2f9a9c8f2804a3c3a85fe163a3d2a54573748f5d
alexshi0000 <yuanshi948@gmail.com>
2020-01-02 21:01:29 -0500

    .

--- js/script.js
+++ js/script.js
@@ -70,11 +70,9 @@ window.addEventListener('scroll', function() {
 
 function load_animators() {
 	//set entire page visible
-	window.addEventListener('load', () => {
-		document.getElementById("page").style.display = "block";
-		document.getElementById("loading").style.display = "none";
-	});
-	
+	document.getElementById("page").style.display = "block";
+	document.getElementById("loading").style.display = "none";
+
   load_progress();
   load_timeline();
   load_projects();
